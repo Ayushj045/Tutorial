c2f7d5f96811970636146bba8bfe10e6ec9188b9
danascape <danascape@gmail.com>
2022-02-13 11:49:18 +0530

    Button Counter

--- app/src/main/java/dev/danascape/tutorial/MainActivity.kt
+++ app/src/main/java/dev/danascape/tutorial/MainActivity.kt
@@ -1,31 +1,25 @@
 package dev.danascape.tutorial
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnApply = findViewById<Button>(R.id.btnApply) // IDK why
-        val etFirstName = findViewById<EditText>(R.id.etFIrstName)
-        val etLastName = findViewById<EditText>(R.id.etLastName)
-        val etBirthDate = findViewById<EditText>(R.id.etBirthDate)
-        val etCountry = findViewById<EditText>(R.id.etCountry)
+        var count = 0
 
+        val btnCount = findViewById<Button>(R.id.btnCount) // IDK why
+        val tvCount = findViewById<TextView>(R.id.tvCount)
 
-        btnApply.setOnClickListener {
-            val firstName = etFirstName.text.toString()
-            val lastName = etLastName.text.toString()
-            val birthDate = etBirthDate.text.toString()
-            val country = etCountry.text.toString()
-            Log.d("MainActivity", "$firstName $lastName, born on $birthDate from $country")
-
+        btnCount.setOnClickListener {
+            count ++
+            tvCount.text = "Let's Count: $count"
         }
 
     }
