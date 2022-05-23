2fa2bb09ddcf9d5c3f4a388286b484ffcc5de405
danascape <danascape@gmail.com>
2022-05-23 16:37:09 +0530

    app: get back to the old

--- app/src/main/java/dev/danascape/tutorial/MainActivity.kt
+++ app/src/main/java/dev/danascape/tutorial/MainActivity.kt
@@ -1,31 +1,11 @@
 package dev.danascape.tutorial
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btnToast = findViewById<Button>(R.id.btnToast)
-        val btnSecond = findViewById<Button>(R.id.btnSecond)
-
-        // apply an onClickListener() method
-        btnToast.setOnClickListener{
-            Toast(this).showCustomToast ("Hello! This is a custom Toast!", this)
-        }
-
-        btnSecond.setOnClickListener {
-            Intent(this, SecondActivity::class.java).also {
-                startActivity(it)
-            }
-        }
     }
 }
