feae0f43ef2a2113ee8eabe46bebec3514d4ac00
danascape <danascape@gmail.com>
2022-06-02 01:59:46 +0530

    Complex way of toast

--- app/src/main/java/dev/danascape/tutorial/MainActivity.kt
+++ app/src/main/java/dev/danascape/tutorial/MainActivity.kt
@@ -1,11 +1,21 @@
 package dev.danascape.tutorial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnShowToast = findViewById<Button>(R.id.btnShowToast)
+
+        btnShowToast.setOnLongClickListener {
+            toast {
+                "hello"
+            }
+            true
+        }
     }
 }
