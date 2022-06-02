d3d43b7335aaadd43f045951b51a5cbda2c54634
danascape <danascape@gmail.com>
2022-06-03 01:04:07 +0530

    Code format

--- app/src/main/java/dev/danascape/tutorial/Person.kt
+++ app/src/main/java/dev/danascape/tutorial/Person.kt
@@ -2,5 +2,4 @@ package dev.danascape.tutorial
 
 import java.io.Serializable
 
-data class Person(val name: String, val age: Int, val country: String) : Serializable {
-}
+data class Person(val name: String, val age: Int, val country: String) : Serializable
