2291e8ff8c1174397384e600ff46d650af758c8d
danascape <danascape@gmail.com>
2022-06-03 18:24:39 +0530

    Add a toolbar menu

--- app/src/main/java/dev/danascape/tutorial/MainActivity.kt
+++ app/src/main/java/dev/danascape/tutorial/MainActivity.kt
@@ -2,6 +2,8 @@ package dev.danascape.tutorial
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,22 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnPhoto = findViewById<Button>(R.id.btnPhoto)
-        val ivPhoto = findViewById<ImageView>(R.id.ivPhoto)
-        btnPhoto.setOnClickListener {
-            Intent(Intent.ACTION_GET_CONTENT).also {
-                it.type = "image/*"
-                startActivityForResult(it, 0)
-            }
-        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val ivPhoto = findViewById<ImageView>(R.id.ivPhoto)
-        if (resultCode == RESULT_OK && requestCode == 0) {
-            val uri = data?.data
-            ivPhoto.setImageURI(uri)
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miAddContact -> Toast.makeText(this, "You clicked on Add Contact", Toast.LENGTH_SHORT).show()
+            R.id.miFavorites -> Toast.makeText(this, "You clicked on Favorites", Toast.LENGTH_SHORT).show()
+            R.id.miSettings -> Toast.makeText(this, "You clicked on Settings", Toast.LENGTH_SHORT).show()
+            R.id.miClose -> Toast.makeText(this, "Finish", Toast.LENGTH_SHORT).show()
+            R.id.miFeedback -> Toast.makeText(this, "You clicked on Feedback", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
 }
