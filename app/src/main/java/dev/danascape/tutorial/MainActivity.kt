0e193e276d63e37313fcdb99b85f00d0090a5b0b
danascape <danascape@gmail.com>
2022-06-08 22:37:39 +0530

    Add Dialog of type 1

--- app/src/main/java/dev/danascape/tutorial/MainActivity.kt
+++ app/src/main/java/dev/danascape/tutorial/MainActivity.kt
@@ -4,12 +4,31 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDialog1 = findViewById<Button>(R.id.btnDialog1)
+        val addContactDialog = AlertDialog.Builder(this)
+            .setTitle("Add Contact")
+            .setMessage("Add Saalim to your contact list")
+            .setIcon(R.drawable.ic_add_contact)
+            .setPositiveButton("Yes") { dialogInterface, i ->
+                Toast.makeText(this, "You Added Saalim to your contact list", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Yes") { dialogInterface, i ->
+                Toast.makeText(this, "You didn't add Saalim to your contact list", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        btnDialog1.setOnClickListener() {
+            addContactDialog.show()
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
